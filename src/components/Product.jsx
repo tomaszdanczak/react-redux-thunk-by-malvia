dbd693e0491766ad09bae246c3198736a7ec818d
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 17:14:10 +0200

    feat: add <Link><Link/> component to the <Product /> component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,19 @@
 import Card from "./Card";
+import { Link } from "react-router-dom";
 
 export default function Product({ product }) {
   const { id, title, image } = product;
 
   return (
     <Card>
-      <div className="h-64 p-5 flex justify-center">
-        <img className="max-w-full h-full" src={image} alt={title} />
-      </div>
-      <div className="border-t-2 p-4">
-        <h2 className="font-semibold text-lg">{title}</h2>
-      </div>
+      <Link to={`/product/${id}`}>
+        <div className="h-64 p-5 flex justify-center">
+          <img className="max-w-full h-full" src={image} alt={title} />
+        </div>
+        <div className="border-t-2 p-4">
+          <h2 className="font-semibold text-lg">{title}</h2>
+        </div>
+      </Link>
     </Card>
   );
 }
