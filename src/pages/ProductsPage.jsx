da8eea90985da2a91f312304202d508e77782dcf
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 16:35:47 +0200

    feat: fetch products from fakestoreapi and save it in store

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,3 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAllProducts } from "../redux/actions/allProductsActions";
+
 export default function ProductsPage() {
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const { data } = await axios.get("https://fakestoreapi.com/products");
+
+    dispatch(setAllProducts(data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return <div>ProductsPage</div>;
 }
