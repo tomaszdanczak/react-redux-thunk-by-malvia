f7636c23c4a213990e9158494854690b1a2e0ef8
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 18:49:10 +0200

    fix: change value of type in removeSelectedProduct action

--- src/redux/actions/selectedProductActions.js
+++ src/redux/actions/selectedProductActions.js
@@ -9,6 +9,6 @@ export const setSelectedProduct = (product) => {
 
 export const removeSelectedProduct = () => {
   return {
-    type: ActionTypes.SET_SELECTED_PRODUCT,
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
   };
 };
