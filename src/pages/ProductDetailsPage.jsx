fd0c4c961d9591b66e571f6ac05f57728b494ac9
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 18:16:04 +0200

    feat: fetch product details data in <ProductDetailsPage /> and save it in the store

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,3 +1,24 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setSelectedProduct } from "../redux/actions/selectedProductActions";
+
 export default function ProductDetailsPage() {
+  const { productId } = useParams();
+  const dispatch = useDispatch();
+
+  const featchProductDetails = async () => {
+    const { data } = await axios.get(
+      `http://fakestoreapi.com/products/${productId}`
+    );
+
+    dispatch(setSelectedProduct(data));
+  };
+
+  useEffect(() => {
+    featchProductDetails();
+  }, [productId]);
+
   return <div>ProductDetailsPage</div>;
 }
