fe6694887f20e12f77172349b8af997eaaad806a
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 16:15:33 +0200

    feat: add <Header/> component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="fixed inset-0 w-full z-50 h-14 pt-3 bg-white border shadow">
+      <div className="w-full md:w-11/12 md:my-0 md:mx-auto lg:w-9/12">
+        <h2 className="text-2xl font-medium">FakeShop</h2>
+      </div>
+    </div>
+  );
+}
