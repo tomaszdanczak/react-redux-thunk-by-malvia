1f5a6f37ef0c2aa983539e5817d7299b255b9fee
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 13:39:05 +0200

    feat: create redux reducer: selectedProductReducer.js

---
+++ src/redux/reducers/selectedProductReducer.js
@@ -0,0 +1,14 @@
+import { ActionTypes } from "../constants/action-types";
+
+export const selectedProductReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_SELECTED_PRODUCT:
+      return { ...payload };
+
+    case ActionTypes.REMOVE_SELECTED_PRODUCT:
+      return {};
+
+    default:
+      return state;
+  }
+};
