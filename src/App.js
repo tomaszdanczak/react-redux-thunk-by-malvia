68cb6e5cd28f9c1ae7789cc72980b4a4c141ea11
Tomasz Dańczak <43489014+tomaszdanczak@users.noreply.github.com>
2021-10-25 16:12:15 +0200

    feat: add routing to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={ProductsPage} />
+        <Route
+          path="/product/:productId"
+          exact
+          component={ProductDetailsPage}
+        />
+        <Route>404 Not Found</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
